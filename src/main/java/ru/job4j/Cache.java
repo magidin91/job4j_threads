34527487c5be3d5f45ebb815666aee2321c3e8d8
magidin91 <magidin@mail.ru>
2021-04-06 04:41:39 +0500

    3. Immutable объекты [#498629]

---
+++ src/main/java/ru/job4j/Cache.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Cache {
+    private final ConcurrentHashMap<Integer, String> dic = new ConcurrentHashMap<>();
+    private final AtomicInteger ids = new AtomicInteger();
+    
+    public Cache() {
+        dic.put(ids.incrementAndGet(), "Petr Arsentev");
+        dic.put(ids.incrementAndGet(), "Ivan Ivanov");
+    }
+    
+    public void add(String name) {
+        dic.put(ids.incrementAndGet(), name);
+    }
+    
+    public boolean contains(String name) {
+        return dic.containsValue(name);
+    }
+}
