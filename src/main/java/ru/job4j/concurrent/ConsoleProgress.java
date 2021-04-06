02467e1985b4b1319f0b9045508be2f0df7dd34d
magidin91 <magidin@mail.ru>
2021-04-06 23:07:10 +0500

    3. Прерывание нити [#498625]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -14,7 +14,7 @@ public class ConsoleProgress implements Runnable {
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
-                break;
+                Thread.currentThread().interrupt();
             }
         }
     }
