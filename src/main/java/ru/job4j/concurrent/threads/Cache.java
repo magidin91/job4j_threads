3646977085a5c1e1f2aec74100b7b8fc916d8f95
magidin91 <magidin@mail.ru>
2021-04-12 03:46:41 +0500

    refactoring

--- src/main/java/ru/job4j/concurrent/threads/Cache.java
+++ src/main/java/ru/job4j/concurrent/threads/Cache.java
@@ -1,22 +1,12 @@
 package ru.job4j.concurrent.threads;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+public final class Cache {
+    private static Cache cache;
 
-public class Cache {
-    private final ConcurrentHashMap<Integer, String> dic = new ConcurrentHashMap<>();
-    private final AtomicInteger ids = new AtomicInteger();
-    
-    public Cache() {
-        dic.put(ids.incrementAndGet(), "Petr Arsentev");
-        dic.put(ids.incrementAndGet(), "Ivan Ivanov");
-    }
-    
-    public void add(String name) {
-        dic.put(ids.incrementAndGet(), name);
-    }
-    
-    public boolean contains(String name) {
-        return dic.containsValue(name);
+    public static synchronized Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
     }
 }
