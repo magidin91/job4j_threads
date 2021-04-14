02aa40f08f6b6f111357b4c4dbe6daa04caedca2
magidin91 <magidin@mail.ru>
2021-04-14 23:30:42 +0500

    Squashed commit of the following:
    4. Скачивание файла с ограничением. [#498620]

---
+++ src/main/java/ru/job4j/concurrent/threads/LoadingBar.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent.threads;
+
+public class LoadingBar {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(
+                () -> {
+                    for (int index = 0; index < 101; index++) {
+                        try {
+                            System.out.print("\rLoading : " + index + "%");
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        thread.start();
+        Thread.sleep(5000);
+        thread.interrupt();
+    }
+}
