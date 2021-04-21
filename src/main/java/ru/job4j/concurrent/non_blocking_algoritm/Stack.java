1edb2f1f70003386776ad0d1b3beb37e0f21073d
magidin91 <magidin@mail.ru>
2021-04-21 20:47:29 +0500

    0. CAS - операции [#498645]

---
+++ src/main/java/ru/job4j/concurrent/non_blocking_algoritm/Stack.java
@@ -0,0 +1,41 @@
+package ru.job4j.concurrent.non_blocking_algoritm;
+
+import net.jcip.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public class Stack<T> {
+    private Node<T> head;
+
+    public void push(T value) {
+        Node<T> temp = new Node<>(value);
+        if (head == null) {
+            head = temp;
+            return;
+        }
+        temp.next = head;
+        head = temp;
+    }
+
+    public T poll() {
+        Node<T> temp = head;
+        /* check-then-act (проверка на null, а затем выбрасывание исключения),
+        , не является атомарной */
+        if (temp == null) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        head = temp.next;
+        temp.next = null;
+        return temp.value;
+    }
+
+
+    private static final class Node<T> {
+        private final T value;
+
+        private Node<T> next;
+
+        public Node(final T value) {
+            this.value = value;
+        }
+    }
+}
