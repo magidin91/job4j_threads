eef342ba8c46a5efb122faeb0c91cc09d3ee9ef1
magidin91 <magidin@mail.ru>
2021-05-04 01:05:39 +0500

    2. Обеспечить остановку потребителя. [#498615]

---
+++ src/main/java/ru/job4j/concurrent/synchronization/threadsafe/SingleLockList.java
@@ -0,0 +1,39 @@
+package ru.job4j.concurrent.synchronization.threadsafe;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Делаем аналог Collections.synchronizedList(), делаем обертку над коллекцией типа List.
+ */
+@ThreadSafe
+public class SingleLockList<T> implements Iterable<T> {
+    @GuardedBy("this")
+    private final List<T> list;
+
+    public SingleLockList(List<T> list) {
+        this.list = (List) list.clone();
+    }
+
+    public synchronized void add(T value) {
+        list.add(value);
+    }
+
+    public synchronized T get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public synchronized Iterator<T> iterator() {
+        return copy(this.list).iterator();
+    }
+
+    private Iterable<T> copy(List<T> list) {
+
+        return new ArrayList<>(list);
+    }
+}
