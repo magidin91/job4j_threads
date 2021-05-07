924e69171b5cdd20c77a6c3f336ce1613dbdada7
magidin91 <magidin@mail.ru>
2021-05-08 04:02:46 +0500

    2. ExecutorService рассылка почты. [#498609]

---
+++ src/main/java/ru/job4j/concurrent/threadpools/executorservice/EmailNotification.java
@@ -0,0 +1,31 @@
+package ru.job4j.concurrent.threadpools.executorservice;
+
+import java.util.concurrent.ExecutorService;
+
+public class EmailNotification {
+    ExecutorService executorService;
+
+    public EmailNotification(ExecutorService executorService) {
+        this.executorService = executorService;
+    }
+
+    public void emailTo(User user) {
+        executorService.submit(() -> {
+            String userName = user.getUsername();
+            String email = user.getEmail();
+
+            String subject = String.format("Notification %s to email %s.", userName, email);
+            String body = String.format("Add a new event to %s", userName);
+
+            send(subject, body, email);
+        });
+
+    }
+
+    public void send(String subject, String body, String email) {
+    }
+
+    public void close() {
+        executorService.shutdown();
+    }
+}
